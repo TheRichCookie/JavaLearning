4435a29612e4669b00d48faec82271e34a5fc221
TheRichCookie <amirsalehi138080@gmail.com>
2022-06-19 16:37:50 +0430

    Using Objects

--- src/main/java/org/JavaTutorial/Main.java
+++ src/main/java/org/JavaTutorial/Main.java
@@ -1,31 +1,101 @@
 package org.JavaTutorial;
+import java.util.Scanner;
+
 
 public class Main{
 
 
+
+
     public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        class bank{
+            int accountid;
+            String accountname;
+            String accountlastname;
+            long balance;
+            String address;
 
-        class phone{
-            double version;
-            int java_learning;
-            String model;
-            phone(double paramversion, String parammodel){
-                model = parammodel;
-                version = paramversion;
-                talk();
 
+            public bank(int accountid, String accountname, String accountlastname, long balance, String address) {
+                this.accountid = accountid;
+                this.accountname = accountname;
+                this.accountlastname = accountlastname;
+                this.balance = balance;
+                this.address = address;
             }
-            void talk(){
-                System.out.println("Your phone model: " + model +"\nVersion: " + version);
+
+
+            public int getAccountid() {
+                return accountid;
             }
 
+            public void setAccountid(int accountid) {
+                this.accountid = accountid;
+            }
+
+            public String getAccountname() {
+                return accountname;
+            }
+
+            public void setAccountname(String accountname) {
+                this.accountname = accountname;
+            }
+
+            public String getAccountlastname() {
+                return accountlastname;
+            }
+
+            public void setAccountlastname(String accountlastname) {
+                this.accountlastname = accountlastname;
+            }
+
+            public long getBalance() {
+                return balance;
+            }
+
+            public void setBalance(long balance) {
+                this.balance = balance;
+            }
+
+            public String getAddress() {
+                return address;
+            }
+
+            public void setAddress(String address) {
+                this.address = address;
+            }
+            public long depositbalance(long x){
+                return balance += x;
+            }
+            public long withdrawalbalance(long x){
+                return balance -= x;
+            }
         }
-        phone phone1 = new phone(1.5, "Android");
-        if(phone1.version == 1.1)
-            System.out.println("Last Version!");
-        else
-            System.out.println("Your phone need update!");
-        phone phone2 = new phone (2.5, "ios");
+        bank bank1 = new bank(10, "Amir", "Salehi", 1500, "Bandar Abbas");
+        System.out.println("Account ID: " + bank1.getAccountid() + "\n" + "Account name: " + bank1.getAccountname() + "\n" + "Account lastname: " + bank1.getAccountlastname() + "\n" + "Balance: " + bank1.getBalance());
+        System.out.print("Want to deposit or withdrawal? ");
+        String depositORwithdrawal = scan.next();
+        if(depositORwithdrawal.equals("deposit")){
+            System.out.print("deposit amount: ");
+            bank1.depositbalance(scan.nextLong());
+            System.out.println("New balance: " + bank1.getBalance());
+        }else if(depositORwithdrawal.equals("withdrawal")){
+            System.out.print("withdrawal amount: ");
+            long withdrawalbalance = scan.nextLong();
+            bank1.withdrawalbalance(withdrawalbalance);
+            if(withdrawalbalance > bank1.getBalance())
+            {
+                System.out.println("You are broke!");
+            }else
+                System.out.println("New balance: " + bank1.getBalance());
+        }
+
+
+
+
+
+
 
 
 
